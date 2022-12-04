bf1344917547b8484cd514f8052ed13141d599dd
rootliveDS <Daniil.Ageevv@gmail.com>
2022-12-04 23:05:40 +0300

    add postgres and edit model

--- nowen/models.py
+++ nowen/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    body = models.TextField
+    body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
